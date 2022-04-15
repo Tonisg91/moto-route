21e67a6bbab87159b5643c6892fbd0d32731bd26
Tonisg91 <sancheztoni.garcia@gmail.com>
2022-04-15 12:26:10 +0200

    refactor

--- App.tsx
+++ App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
-import HomeScreen from './src/views/HomeScreen';
-import SettingsScreen from './src/views/SettingsScreen';
+import HomeScreen from './src/components/Map';
+import SettingsScreen from './src/components/Settings';
 import {StyleSheet} from 'react-native';
 
 const Tab = createMaterialBottomTabNavigator();
@@ -11,28 +11,36 @@ const Tab = createMaterialBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        barStyle={styles.barStyle}
-        activeColor="tomato"
-        inactiveColor="black"
-        screenOptions={({route}) => ({
-          tabBarIcon: ({focused, color}) => {
-            let iconName = 'map-outline';
-
-            if (route.name === 'Map') {
-              iconName = focused ? 'map' : 'map-outline';
-            } else if (route.name === 'Settings') {
-              iconName = focused ? 'settings' : 'settings-outline';
-            }
-
-            // You can return any component that you like here!
-            return <Ionicons name={iconName} size={20} color={color} />;
-          },
-        })}>
+      <CustomNavigator>
         <Tab.Screen name="Map" component={HomeScreen} />
         <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
+      </CustomNavigator>
     </NavigationContainer>
+  );
+}
+
+function CustomNavigator({children}: {children: React.ReactNode}) {
+  return (
+    <Tab.Navigator
+      barStyle={styles.barStyle}
+      activeColor="tomato"
+      inactiveColor="black"
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color}) => {
+          let iconName = 'map-outline';
+
+          if (route.name === 'Map') {
+            iconName = focused ? 'map' : 'map-outline';
+          } else if (route.name === 'Settings') {
+            iconName = focused ? 'settings' : 'settings-outline';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={20} color={color} />;
+        },
+      })}>
+      {children}
+    </Tab.Navigator>
   );
 }
 
