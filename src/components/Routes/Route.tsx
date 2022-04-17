59c204d0671c9178c523f2012eb44bbde74c7c21
Tonisg91 <sancheztoni.garcia@gmail.com>
2022-04-17 20:09:58 +0200

    Route class

---
+++ src/components/Routes/Route.tsx
@@ -0,0 +1,15 @@
+export default class Route {
+  constructor() {}
+
+  startRoute() {
+    console.log('Route started')
+  }
+
+  endRoute() {
+    console.log('Route ended')
+  }
+
+  pauseRoute() {
+    console.log('Route paused')
+  }
+}
