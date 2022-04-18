a3d6c6bc6980d4d18831b9b243d53b1d3f2695c3
Tonisg91 <sancheztoni.garcia@gmail.com>
2022-04-18 13:36:23 +0200

    Route class v1

--- src/components/Routes/Route.tsx
+++ src/components/Routes/Route.tsx
@@ -1,15 +1,48 @@
-export default class Route {
-  constructor() {}
+const RECORDING = 'RECORDING'
+const PAUSED = 'PAUSED'
+const STOPPED = 'STOPPED'
+export interface RoutePath {
+  coordinates: [string, string]
+  speed: number
+  timestamp: number
+}
 
-  startRoute() {
-    console.log('Route started')
+export interface RouteProps {
+  path: RoutePath[]
+  user: string // firebase uid
+  status: 'RECORDING' | 'PAUSED' | 'STOPPED'
+}
+
+export default class Route {
+  path: RoutePath[]
+  user: string
+  private status: 'RECORDING' | 'PAUSED' | 'STOPPED'
+
+  constructor(userId: string) {
+    this.user = userId
+    this.path = []
+    this.status = STOPPED
+  }
+
+  private changeStatus(status: 'RECORDING' | 'PAUSED' | 'STOPPED') {
+    this.status = status
+  }
+
+  addPoint(point: RoutePath) {
+    if (this.status !== RECORDING) return
+    this.path.push(point)
+  }
+
+  startRoute(initialPoint: RoutePath) {
+    this.changeStatus(RECORDING)
+    if (this.path.length === 0) this.path.push(initialPoint)
   }
 
   endRoute() {
-    console.log('Route ended')
+    this.changeStatus(STOPPED)
   }
 
   pauseRoute() {
-    console.log('Route paused')
+    this.changeStatus(PAUSED)
   }
 }
